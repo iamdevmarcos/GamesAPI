d2afab14a7ef9fd53822d15cf398671512b3f04c
Marcos Mendes <marcosdev.me@gmail.com>
2023-07-11 07:51:43 -0300

    feat: create Game class

---
+++ src/main/java/com/marcosmendes/gameslist/entities/Game.java
@@ -0,0 +1,45 @@
+package com.marcosmendes.gameslist.entities;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_game")
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+
+    @Column(name = "game_year")
+    private Integer year;
+
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        if (getClass() != object.getClass()) return false;
+
+        Game other = (Game) object;
+        return Objects.equals(id, other.id);
+    }
+}
